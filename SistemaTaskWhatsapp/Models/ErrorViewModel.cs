903f5bd7c740a0cfa6984938cebc41f54f7facc4
Jorge <jorge---9511@hotmail.com>
2025-12-18 14:02:57 -0600

    Agregar archivos de proyecto.

---
+++ SistemaTaskWhatsapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaTaskWhatsapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
