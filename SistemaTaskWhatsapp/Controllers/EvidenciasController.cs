a5ac0c65c70aa5d183418d0c66e5b6d970ce0dcb
Orlando <9697j@ORLANDO>
2026-01-05 11:39:34 -0600

    feat: Se agregó la vista para las evidencias

---
+++ SistemaTaskWhatsapp/Controllers/EvidenciasController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaTaskWhatsapp.AccesoDatos.Data.Repository.IRepository;
+using SistemaTaskWhatsapp.Models;
+using SistemaTaskWhatsapp.Utilidades;
+
+namespace SistemaTaskWhatsapp.Controllers
+{
+    public class EvidenciasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public EvidenciasController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var listaEvidencias = await _contenedorTrabajo.Evidencia.GetAllAsync();
+
+            return View(listaEvidencias);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Evidencia model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var existeNombre = await _contenedorTrabajo.Evidencia.GetFirstOrDefaultAsync(e => e.Id == model.Id);
+
+            if (existeNombre != null)
+            {
+                ModelState.AddModelError("", "Ya existe una evidencia con ese id");
+                return View(model);
+            }
+
+            await _contenedorTrabajo.Evidencia.AddAsync(model);
+            await _contenedorTrabajo.SaveAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _contenedorTrabajo.Evidencia.GetByIdAsync(id);
+
+            if (model == null) return RedirectToAction("Index");
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Evidencia model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _contenedorTrabajo.Evidencia.Update(model);
+            await _contenedorTrabajo.SaveAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var EvidenciaEliminar = await _contenedorTrabajo.Evidencia.GetByIdAsync(id);
+
+            if (EvidenciaEliminar == null) return RedirectToAction("Index");
+
+            _contenedorTrabajo.Evidencia.Remove(EvidenciaEliminar);
+            await _contenedorTrabajo.SaveAsync();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ SistemaTaskWhatsapp/wwwroot/js/Evidencia/EmpleadoIndex.js
@@ -0,0 +1,10 @@
+﻿//Mostrar datos y mandar id al modal eliminar
+const modalEliminar = document.getElementById("modalEliminar");
+
+modalEliminar.addEventListener('show.bs.modal', (e) => {
+    const boton = e.relatedTarget;
+    const id = boton.getAttribute('data-id');
+
+    modalEliminar.querySelector('#inputId').value = id;
+
+});
