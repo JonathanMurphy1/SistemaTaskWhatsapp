322bab960669e838dd1dc5890f12ff9189f807f9
Jorge <jorge---9511@hotmail.com>
2026-01-12 09:55:18 -0600

    feat: Se crea la interfaz del servicio de flujo para el supervisor, y se inyecta

---
+++ SistemaTaskWhatsapp.Services/SupervisorFlowService.cs
@@ -0,0 +1,24 @@
+﻿using SistemaTaskWhatsapp.AccesoDatos.Data.Repository.IRepository;
+using SistemaTaskWhatsapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTaskWhatsapp.Services
+{
+    public class SupervisorFlowService : ISupervisorFlowService
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public SupervisorFlowService(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        public Task<string> ProcesarAsync(Usuario usuario, ChatSession sesion, string mensaje)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SistemaTaskWhatsapp/Program.cs
+++ SistemaTaskWhatsapp/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IContenedorTrabajo, ContenedorTrabajo>();
 
 builder.Services.AddScoped<WhatsAppFlowService>();
+builder.Services.AddScoped<ISupervisorFlowService, SupervisorFlowService>();
 
 //Registrar servicio de whatsapp
 builder.Services.AddSingleton(new WhatsAppService(
