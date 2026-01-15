947c4c36a51b2cbdd1044f2886a0a07c6af566db
Jorge <jorge---9511@hotmail.com>
2026-01-15 11:27:40 -0600

    feat: Se agregan campos a la sesion de chat para verificar con token

---
+++ SistemaTaskWhatsapp.AccesoDatos/Migrations/20260115172523_Se agregan los campos TokenFormularios y FechaCreacionToken al modelo ChatSesion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaTaskWhatsapp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeagreganloscamposTokenFormulariosyFechaCreacionTokenalmodeloChatSesion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaCreacionToken",
+                table: "ChatSession",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TokenFormularios",
+                table: "ChatSession",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaCreacionToken",
+                table: "ChatSession");
+
+            migrationBuilder.DropColumn(
+                name: "TokenFormularios",
+                table: "ChatSession");
+        }
+    }
+}
