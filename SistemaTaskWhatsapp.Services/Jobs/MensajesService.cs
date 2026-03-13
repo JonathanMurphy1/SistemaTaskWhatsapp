5d936c6777bbfec0cbb10d0df6e304962fc8cd34
Orlando <9697j@ORLANDO>
2026-03-13 12:02:09 -0600

    feat: Se implementó Hangfire para el envió automatico de mensajes, de momento solo funciona en consola

---
+++ SistemaTaskWhatsapp.Services/Jobs/MensajesService.cs
@@ -0,0 +1,31 @@
+﻿using SistemaTaskWhatsapp.AccesoDatos.Data.Repository.IRepository;
+using System.Threading.Tasks;
+
+namespace SistemaTaskWhatsapp.Services.Jobs
+{
+    public class MessageJobs
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public MessageJobs(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+        // Job para recordatorios a empleados
+        public async Task EnviarRecordatoriosEmpleados()
+        {
+            MensajesService servicio = new MensajesService(_contenedorTrabajo);
+
+            await servicio.EnviarRecordatoriosTareas();
+        }
+
+        // Job para avisar a supervisores
+        public async Task AvisarSupervisores()
+        {
+            MensajesService servicio = new MensajesService(_contenedorTrabajo);
+
+            await servicio.AvisarSupervisoresReportes();
+        }
+    }
+}
--- SistemaTaskWhatsapp/Program.cs
+++ SistemaTaskWhatsapp/Program.cs
@@ -1,3 +1,5 @@
+using Hangfire;
+using Hangfire.SqlServer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SistemaTaskWhatsapp.AccesoDatos.Data.Repository;
@@ -6,6 +8,7 @@ using SistemaTaskWhatsapp.AccesoDatos.Data.Seed;
 using SistemaTaskWhatsapp.Data;
 using SistemaTaskWhatsapp.Models;
 using SistemaTaskWhatsapp.Services;
+using SistemaTaskWhatsapp.Services.Jobs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,12 +33,21 @@ builder.Services.AddScoped<ISupervisorFlowService, SupervisorFlowService>();
 
 builder.Services.AddScoped<IEmpleadoFlowService, EmpleadoFlowService>();
 
+builder.Services.AddScoped<MessageJobs>();
+builder.Services.AddScoped<MensajesService>();
+
 //Registrar servicio de whatsapp
 builder.Services.AddSingleton(new WhatsAppService(
     builder.Configuration["Twilio:AccountSid"],
     builder.Configuration["Twilio:AuthToken"],
     builder.Configuration["Twilio:WhatsAppNumber"]
 ));
+
+//Función para enviar mensajes de manera automatica
+builder.Services.AddHangfire(config =>
+    config.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
@@ -61,6 +73,22 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseHangfireDashboard();
+
+RecurringJob.AddOrUpdate<MessageJobs>(
+    "recordatorio-empleados",
+    job => job.EnviarRecordatoriosEmpleados(),
+    Cron.Daily(12),
+    TimeZoneInfo.Local
+);
+
+RecurringJob.AddOrUpdate<MessageJobs>(
+    "avisar-supervisores",
+    job => job.AvisarSupervisores(),
+    Cron.Daily(12),
+    TimeZoneInfo.Local
+);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
