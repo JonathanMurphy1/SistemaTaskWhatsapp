6e8a337af95bef35c35eded04333e790978554bc
Orlando <9697j@ORLANDO>
2026-03-13 09:33:15 -0600

    feat: Se agrega un archivo en utilidades para tener mensajes predeterminados

---
+++ SistemaTaskWhatsapp.Utilidades/SystemMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SistemaTaskWhatsapp.Utilidades
+{
+    public static class SystemMessages
+    {
+        // Recordatorio para empleados sobre tareas pendientes
+        public const string RecordatorioTareas =
+            "Buenos días {0}, tienes {1} tareas pendientes hoy. Recuerda completarlas antes de finalizar tu jornada.";
+
+        // Aviso para supervisores cuando un empleado tiene reportes por revisar
+        public const string AvisoSupervisorTareas =
+            "Aviso: Buen día {0} tienes {1} reportes sin revisar.";
+
+
+    }
+}
