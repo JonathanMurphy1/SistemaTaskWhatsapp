a2b7f4021f8b2d98c682e96985fb425e4b8ee144
Orlando <9697j@ORLANDO>
2026-04-28 10:42:11 -0600

    feat: Se implementa la opción de día especifico para la función de mensajes

--- SistemaTaskWhatsapp.Utilidades/CronHelper.cs
+++ SistemaTaskWhatsapp.Utilidades/CronHelper.cs
@@ -20,20 +20,42 @@ namespace SistemaTaskWhatsapp.Utilidades
 
             var minuto = partes[0];
             var hora = partes[1];
-            var dias = partes[4];
+            var diaMes = partes[2];
+            var mes = partes[3];
+            var diaSemana = partes[4];
 
-            string frecuencia = dias switch
+
+            string frecuencia = "";
+
+            //Detectar tipo de cron
+            if (diaMes != "*" && mes != "*")
             {
-                "*" => "Todos los días",
-                "1-5" => "Lunes a viernes",
-                "1" => "Cada lunes",
-                _ => "Frecuencia personalizada"
-            };
+                //Anual (día y mes definidos)
+                if (int.TryParse(diaMes, out int d) && int.TryParse(mes, out int mesNumero))
+                {
+                    var fecha = new DateTime(2000, mesNumero, d);
+                    frecuencia = $"Cada {fecha.ToString("dd 'de' MMMM")}";
+                }
+                else
+                {
+                    frecuencia = "Fecha específica";
+                }
+            }
+            else
+            {
+                frecuencia = diaSemana switch
+                {
+                    "*" => "Todos los días",
+                    "1-5" => "Lunes a viernes",
+                    "1" => "Cada lunes",
+                    _ => "Frecuencia personalizada"
+                };
+            }
 
             // Formato de hora
-            if (int.TryParse(hora, out int h) && int.TryParse(minuto, out int m))
+            if (int.TryParse(hora, out int h) && int.TryParse(minuto, out int min))
             {
-                var time = new TimeOnly(h, m);
+                var time = new TimeOnly(h, min);
                 return $"{frecuencia} a las {time.ToString("hh:mm tt")}";
             }
 
