d9483b8f9d95f6e42fa35bff3c19aeb4875a36d6
Orlando <9697j@ORLANDO>
2026-04-29 13:24:47 -0600

    feat: Creación de la API de mensajes

---
+++ SistemaTaskWhatsapp.Models/DTOs/MensajeDto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SistemaTaskWhatsapp.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTaskWhatsapp.Models
+{
+    public class MensajeResponseDto
+    {
+        public int Id { get; set; }
+
+        public Roles Tipo { get; set; }
+        public string Contenido { get; set; }
+        public string Cron { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public int EmpresaId { get; set; }
+
+    }
+}
---
+++ SistemaTaskWhatsapp/Controllers/MensajesAPIController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaTaskWhatsapp.AccesoDatos.Data.Repository.IRepository;
+using SistemaTaskWhatsapp.Models;
+using System.Threading.Tasks;
+
+[Route("api/mensajes")]
+[ApiController]
+public class MensajesApiController : ControllerBase
+{
+    private readonly IContenedorTrabajo _contenedorTrabajo;
+
+    public MensajesApiController(IContenedorTrabajo contenedorTrabajo)
+    {
+        _contenedorTrabajo = contenedorTrabajo;
+    }
+
+    //Mostrar datos en un Json
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var lista = await _contenedorTrabajo.Mensaje.GetAllAsync();
+
+        var resultado = lista.Select(m => new MensajeResponseDto
+        {
+            Id = m.Id,
+            Tipo = m.Tipo,
+            Contenido = m.Contenido,
+            Cron = m.Cron,
+            Activo = m.Activo, 
+            FechaCreacion = m.FechaCreacion,
+            EmpresaId = (int)m.EmpresaId
+        });
+
+        return Ok(resultado);
+    }
+}
